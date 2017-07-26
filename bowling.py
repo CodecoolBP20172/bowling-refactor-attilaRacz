6592a4282f6454d6c9ab4ebe753fe713834d00de
Attila Racz <racz.attila89@gmail.com>
2017-07-26 20:56:22 +0200

    comments removed, useless exception handling removed

--- bowling.py
+++ bowling.py
@@ -1,38 +1,35 @@
-def score(game): # az argument az eredmény stringként kifejezve
-    result = 0 # az eredmény számláló
-    frame = 1 # a kör számláló
-    first_roll = True # a kör első gurítása
-    for i in range(len(game)): # végigiterál az eredmény karakterein
-        if game[i] == '/': # ha spare van, a következő kör első gurításának eredménye ide számlálódik bónuszként
-            result += 10 - get_value(game[i-1]) # hozzáadja a következő gurítás első eredményét bónuszként
+def score(game):
+    result = 0
+    frame = 1
+    first_roll = True
+    for i in range(len(game)):
+        if game[i] == '/':
+            result += 10 - get_value(game[i-1])
         else:
-            result += get_value(game[i]) # hozzáadja az aktuális értéket
-        # if not first_roll:
-            # frame += 1
-        if frame < 10  and get_value(game[i]) == 10: # ha 10 kör alatt vagyunk
-            if game[i] in 'xX/': # ha spare van, hozzáadjuk a következő kör első értékét is
+            result += get_value(game[i])
+        if frame < 10  and get_value(game[i]) == 10:
+            if game[i] in 'xX/':
                 result += get_value(game[i+1])
-                if game[i] in 'xX' and game[i+2] == '/': # ha a strike után kettőve spare-t gurítunk, hozzáadjuk a spare értékét
-                    result += 10 - get_value(game[i+1]) # a spare értéke
+                if game[i] in 'xX' and game[i+2] == '/':
+                    result += 10 - get_value(game[i+1])
                 elif game[i] in 'xX' and game[i+2] != '/':
-                    result += get_value(game[i+2]) # hozzáadjuk a következő gurítás értékét is
-        if not first_roll: # a második kör végén hozzáad egyet a körszámlálóhoz
+                    result += get_value(game[i+2])
+        if not first_roll:
             frame += 1
-        if first_roll: # az első gurítás végén átvált a második felére
+        if first_roll:
             first_roll = False
         else:
-            first_roll = True # egyébként hagyja
-        if game[i] in 'xX': # ha strike-ot gurítottunk, a következő kör első gurítása jön
+            first_roll = True
+        if game[i] in 'xX':
             first_roll = True
             frame += 1
     return result
 
-def get_value(char): # az inteket intként adja vissza, a strike-ot, a spare-t a megfelelő értékként
+def get_value(char):
     if char in '123456789':
         return int(char)
     elif char in 'xX/':
         return 10
     elif char == '-':
         return 0
-    else:
-        raise ValueError()
+
